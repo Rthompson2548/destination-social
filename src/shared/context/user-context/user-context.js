3f5af614f8b6f5027f03b0e1e23884e5321d9892
reillythompson <reilly.thompson@webwizrd.com>
2022-02-05 12:59:45 -0800

    Revert "updated"
    This reverts commit c5e926fe40af0bb081c411abf43583f951f1a39e.

--- src/shared/context/user-context/user-context.js
+++ src/shared/context/user-context/user-context.js
@@ -1,9 +1,10 @@
 /** createContext(): 
- * designed to share data that can be considered “global” for a tree 
- * of React components, such as the current authenticated user, theme, 
- * or preferred language 
- */
+     * designed to share data that can be considered “global” for a tree 
+     * of React components, such as the current authenticated user, theme, 
+     * or preferred language 
+*/
 import { createContext } from "react";
 
 /** createContext(defaultValue) */
-createContext({ isLoggedIn: false, login: () })
+createContext({isLoggedIn: false, login: () })
+
--- src/user/components/UserItem/UserItem.js
+++ src/user/components/UserItem/UserItem.js
@@ -5,32 +5,25 @@ import Avatar from "../../../shared/components/UIElements/Avatar/Avatar";
 import Card from "../../../shared/components/UIElements/Card/Card";
 
 const UserItem = (props) => {
-    return ( <
-        li className = "user" >
-        <
-        Card >
-        <
-        Link to = { `/${props.id}/places` } >
-        <
-        div className = "user-container" >
-        <
-        div className = "right" >
-        <
-        Avatar image = { props.image }
-        alt = { props.name }
-        /> <
-        /div> <
-        div className = "user-info" >
-        <
-        h2 className = "user-name" > { props.name } < /h2> <
-        h3 className = "user-places" > { props.placeCount } { props.placeCount === 1 ? "Place" : "Places" } <
-        /h3> <
-        /div> <
-        /div> <
-        /Link> <
-        /Card> <
-        /li>
-    );
+  return (
+    <li className="user">
+      <Card>
+        <Link to={`/${props.id}/places`}>
+          <div className="user-container">
+            <div className="right">
+              <Avatar image={props.image} alt={props.name} />
+            </div>
+            <div className="user-info">
+              <h2 className="user-name">{props.name}</h2>
+              <h3 className="user-places">
+                {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+              </h3>
+            </div>
+          </div>
+        </Link>
+      </Card>
+    </li>
+  );
 };
 
 export default UserItem;
