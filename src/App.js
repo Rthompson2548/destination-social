eedd355d763e677b7c01844c7379774387382113
reillythompson <reilly.thompson@webwizrd.com>
2022-02-05 12:54:41 -0800

    Revert "updated with createContext for users login status"
    This reverts commit 2e4023d59b2d2bfa8df1d4e0b4ecbc103b7de7ce.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Redirect,
-    Switch,
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
 } from "react-router-dom";
-import { UserContext } from "./shared/context/user-context/user-context";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation/MainNavigation";
@@ -15,54 +14,35 @@ import Authenticate from "./user/pages/Authenticate/Authenticate/Authenticate";
 import SignUp from "./user/pages/Authenticate/SignUp/SignUp";
 
 const App = () => {
-    return ( <
-        Router >
-        <
-        MainNavigation / >
-        <
-        main >
-        <
-        Switch >
-        <
-        Route path = "/"
-        exact >
-        <
-        Users / >
-        <
-        /Route> <
-        Route path = "/login" >
-        <
-        Authenticate / >
-        <
-        /Route> <
-        Route path = "/signup" >
-        <
-        SignUp / >
-        <
-        /Route> <
-        Route path = "/:userId/places"
-        exact >
-        <
-        UserPlaces / >
-        <
-        /Route> <
-        Route path = "/places/new" >
-        <
-        NewPlace / >
-        <
-        /Route> <
-        Route path = "/places/:placeId" >
-        <
-        UpdatePlace / >
-        <
-        /Route>
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/login">
+            <Authenticate />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new">
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
 
-        { /* redirects back to home page when users adds anything after "/" */ } <
-        Redirect to = "/" / >
-        <
-        /Switch> < /
-        main > <
-        /Router>
-    );
+          {/* redirects back to home page when users adds anything after "/" */}
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 };
 export default App;
--- src/shared/context/user-context/user-context.js
+++ src/shared/context/user-context/user-context.js
@@ -1,13 +1,10 @@
 /** createContext(): 
- * designed to share data that can be considered “global” for a tree 
- * of React components, such as the current authenticated user, theme, 
- * or preferred language 
- */
+     * designed to share data that can be considered “global” for a tree 
+     * of React components, such as the current authenticated user, theme, 
+     * or preferred language 
+*/
 import { createContext } from "react";
 
-/** createContext(defaultValue, method(s)) */
-export const userContext = createContext({
-    isLoggedIn: false,
-    login: () => {},
-    logout: () => {}
-});
+/** createContext(defaultValue) */
+createContext({isLoggedIn: false, login: () })
+
